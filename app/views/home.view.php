c1a6f9928a28bb8c1886949063fe9afab209de3e
Eslam Elhusseiny <eslam.hamed.elhusseiny@gmail.com>
2024-03-31 03:39:09 +0200

    added news and sponsor section to home page

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -23,331 +23,33 @@
     <main>
       <!-- JOBS -->
 
-      <section class="container jobs-container">
+      <section class="jobs-section">
         <div class="section-heading-wrapper">
           <h2 class="section-heading">Most Popular Jobs</h2>
           <h4 class="section-heading-small">
             Know your worth and find the job that qualify your life
           </h4>
         </div>
-        <div class="job-grid">
-          <div class="job-card">
-            <a href="" class="btn-bookmark">
-              <i class="fa fa-bookmark"></i>
-            </a>
-            <div class="company-logo">
-              <a href="">
-                <img
-                  width="70"
-                  src="https://apusthemes.com/wp-demo/superio/wp-content/uploads/2021/03/y9.jpg"
-                  alt="Company Logo"
-                />
-              </a>
-            </div>
-            <div class="job-content">
-              <div class="title-wrapper">
-                <h3 class="job-title">
-                  <a href=""> Junior Graphic Designer </a>
-                </h3>
-                <span class="featured">Featured</span>
+        <div class="container job-grid">
+          
+        <?php   loadPartial('jobCard'); ?>
+        <?php   loadPartial('jobCard'); ?>
+        <?php   loadPartial('jobCard'); ?>
+        <?php   loadPartial('jobCard'); ?>
+        <?php   loadPartial('jobCard'); ?>
+        <?php   loadPartial('jobCard'); ?>
 
-                <span class="filled">Filled</span>
-              </div>
-              <div class="job-metas">
-                <div class="category-job">
-                  <div class="job-category">
-                    <i class="fa fa-briefcase breif"></i>
-                    <a href="">Design</a>,
-
-                    <a href="">Development</a>
-                  </div>
-                </div>
-                <div class="job-location">
-                  <i class="fa fa-map-location-dot"></i>
-                  <a href="">New York</a>
-                </div>
-                <div class="job-salary">
-                  <i class="fa fa-money-bills"></i>
-                  <span class="suffix">$</span
-                  ><span class="price-text">150</span> -
-                  <span class="suffix">$</span
-                  ><span class="price-text">180</span> / week
-                </div>
-              </div>
-              <div class="job-metas-bottom">
-                <div class="">
-                  <a class="job-type tag" href="">Full Time</a>
-                </div>
-                <span class="urgent tag">Urgent</span>
-              </div>
-            </div>
-          </div>
-          <div class="job-card">
-            <a href="" class="btn-bookmark">
-              <i class="fa fa-bookmark"></i>
-            </a>
-            <div class="company-logo">
-              <a href="">
-                <img
-                  width="70"
-                  src="https://apusthemes.com/wp-demo/superio/wp-content/uploads/2021/03/y9.jpg"
-                  alt="Company Logo"
-                />
-              </a>
-            </div>
-            <div class="job-content">
-              <div class="title-wrapper">
-                <h3 class="job-title">
-                  <a href=""> Junior Graphic Designer </a>
-                </h3>
-                <span class="featured">Featured</span>
-
-                <span class="filled">Filled</span>
-              </div>
-              <div class="job-metas">
-                <div class="category-job">
-                  <div class="job-category">
-                    <i class="fa fa-briefcase breif"></i>
-                    <a href="">Design</a>,
-
-                    <a href="">Development</a>
-                  </div>
-                </div>
-                <div class="job-location">
-                  <i class="fa fa-map-location-dot"></i>
-                  <a href="">New York</a>
-                </div>
-                <div class="job-salary">
-                  <i class="fa fa-money-bills"></i>
-                  <span class="suffix">$</span
-                  ><span class="price-text">150</span> -
-                  <span class="suffix">$</span
-                  ><span class="price-text">180</span> / week
-                </div>
-              </div>
-              <div class="job-metas-bottom">
-                <div class="">
-                  <a class="job-type tag" href="">Full Time</a>
-                </div>
-                <span class="urgent tag">Urgent</span>
-              </div>
-            </div>
-          </div>
-          <div class="job-card">
-            <a href="" class="btn-bookmark">
-              <i class="fa fa-bookmark"></i>
-            </a>
-            <div class="company-logo">
-              <a href="">
-                <img
-                  width="70"
-                  src="https://apusthemes.com/wp-demo/superio/wp-content/uploads/2021/03/y9.jpg"
-                  alt="Company Logo"
-                />
-              </a>
-            </div>
-            <div class="job-content">
-              <div class="title-wrapper">
-                <h3 class="job-title">
-                  <a href=""> Junior Graphic Designer </a>
-                </h3>
-                <span class="featured">Featured</span>
-
-                <span class="filled">Filled</span>
-              </div>
-              <div class="job-metas">
-                <div class="category-job">
-                  <div class="job-category">
-                    <i class="fa fa-briefcase breif"></i>
-                    <a href="">Design</a>,
-
-                    <a href="">Development</a>
-                  </div>
-                </div>
-                <div class="job-location">
-                  <i class="fa fa-map-location-dot"></i>
-                  <a href="">New York</a>
-                </div>
-                <div class="job-salary">
-                  <i class="fa fa-money-bills"></i>
-                  <span class="suffix">$</span
-                  ><span class="price-text">150</span> -
-                  <span class="suffix">$</span
-                  ><span class="price-text">180</span> / week
-                </div>
-              </div>
-              <div class="job-metas-bottom">
-                <div class="">
-                  <a class="job-type tag" href="">Full Time</a>
-                </div>
-                <span class="urgent tag">Urgent</span>
-              </div>
-            </div>
-          </div>
-          <div class="job-card">
-            <a href="" class="btn-bookmark">
-              <i class="fa fa-bookmark"></i>
-            </a>
-            <div class="company-logo">
-              <a href="">
-                <img
-                  width="70"
-                  src="https://apusthemes.com/wp-demo/superio/wp-content/uploads/2021/03/y9.jpg"
-                  alt="Company Logo"
-                />
-              </a>
-            </div>
-            <div class="job-content">
-              <div class="title-wrapper">
-                <h3 class="job-title">
-                  <a href=""> Junior Graphic Designer </a>
-                </h3>
-                <span class="featured">Featured</span>
-
-                <span class="filled">Filled</span>
-              </div>
-              <div class="job-metas">
-                <div class="category-job">
-                  <div class="job-category">
-                    <i class="fa fa-briefcase breif"></i>
-                    <a href="">Design</a>,
-
-                    <a href="">Development</a>
-                  </div>
-                </div>
-                <div class="job-location">
-                  <i class="fa fa-map-location-dot"></i>
-                  <a href="">New York</a>
-                </div>
-                <div class="job-salary">
-                  <i class="fa fa-money-bills"></i>
-                  <span class="suffix">$</span
-                  ><span class="price-text">150</span> -
-                  <span class="suffix">$</span
-                  ><span class="price-text">180</span> / week
-                </div>
-              </div>
-              <div class="job-metas-bottom">
-                <div class="">
-                  <a class="job-type tag" href="">Full Time</a>
-                </div>
-                <span class="urgent tag">Urgent</span>
-              </div>
-            </div>
-          </div>
-          <div class="job-card">
-            <a href="" class="btn-bookmark">
-              <i class="fa fa-bookmark"></i>
-            </a>
-            <div class="company-logo">
-              <a href="">
-                <img
-                  width="70"
-                  src="https://apusthemes.com/wp-demo/superio/wp-content/uploads/2021/03/y9.jpg"
-                  alt="Company Logo"
-                />
-              </a>
-            </div>
-            <div class="job-content">
-              <div class="title-wrapper">
-                <h3 class="job-title">
-                  <a href=""> Junior Graphic Designer </a>
-                </h3>
-                <span class="featured">Featured</span>
-
-                <span class="filled">Filled</span>
-              </div>
-              <div class="job-metas">
-                <div class="category-job">
-                  <div class="job-category">
-                    <i class="fa fa-briefcase breif"></i>
-                    <a href="">Design</a>,
-
-                    <a href="">Development</a>
-                  </div>
-                </div>
-                <div class="job-location">
-                  <i class="fa fa-map-location-dot"></i>
-                  <a href="">New York</a>
-                </div>
-                <div class="job-salary">
-                  <i class="fa fa-money-bills"></i>
-                  <span class="suffix">$</span
-                  ><span class="price-text">150</span> -
-                  <span class="suffix">$</span
-                  ><span class="price-text">180</span> / week
-                </div>
-              </div>
-              <div class="job-metas-bottom">
-                <div class="">
-                  <a class="job-type tag" href="">Full Time</a>
-                </div>
-                <span class="urgent tag">Urgent</span>
-              </div>
-            </div>
-          </div>
-          <div class="job-card">
-            <a href="" class="btn-bookmark">
-              <i class="fa fa-bookmark"></i>
-            </a>
-            <div class="company-logo">
-              <a href="">
-                <img
-                  width="70"
-                  src="https://apusthemes.com/wp-demo/superio/wp-content/uploads/2021/03/y9.jpg"
-                  alt="Company Logo"
-                />
-              </a>
-            </div>
-            <div class="job-content">
-              <div class="title-wrapper">
-                <h3 class="job-title">
-                  <a href=""> Junior Graphic Designer </a>
-                </h3>
-                <span class="featured">Featured</span>
-
-                <span class="filled">Filled</span>
-              </div>
-              <div class="job-metas">
-                <div class="category-job">
-                  <div class="job-category">
-                    <i class="fa fa-briefcase breif"></i>
-                    <a href="">Design</a>,
-
-                    <a href="">Development</a>
-                  </div>
-                </div>
-                <div class="job-location">
-                  <i class="fa fa-map-location-dot"></i>
-                  <a href="">New York</a>
-                </div>
-                <div class="job-salary">
-                  <i class="fa fa-money-bills"></i>
-                  <span class="suffix">$</span
-                  ><span class="price-text">150</span> -
-                  <span class="suffix">$</span
-                  ><span class="price-text">180</span> / week
-                </div>
-              </div>
-              <div class="job-metas-bottom">
-                <div class="">
-                  <a class="job-type tag" href="">Full Time</a>
-                </div>
-                <span class="urgent tag">Urgent</span>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
 
       <!-- HOW IT WORKS -->
 
-      <section class="container how-container">
+      <section class="how-section">
         <div class="section-heading-wrapper">
           <h2 class="section-heading">How It Works?</h2>
           <h4 class="section-heading-small">Job for anyone, anywhere</h4>
         </div>
-        <div class="how-it-works">
+        <div class="container how-it-works">
           <div class="card">
             <div class="how-image">
               <img src="images/how01-acc.png" alt="How It Works Step 1" />
@@ -374,6 +76,75 @@
           </div>
         </div>
       </section>
+
+      <!-- RECENT NEWS ARTICLES -->
+
+      <section class="news-section">
+        <div class="section-heading-wrapper">
+          <h2 class="section-heading">Recent News Articles</h2>
+          <h4 class="section-heading-small">Fresh job related news content posted each day</h4>
+        </div>
+        <div class="container news-container">
+          <div class="news-card">
+            <div class="image">
+              <img src="images/news/blog1.jpg" alt="">
+            </div>
+            <div class="blog-desc">
+              <div class="timestamp">
+                <span>31 March, 2024</span>
+                <span class="seperator">-</span>
+                <span>2 Comments</span>
+              </div>
+              <div class="title">
+                <h4>Attract Sales And Profits</h4>
+                <p>A job ravenously while Far much that one rank beheld after
+                outside...</p>
+                <a href="#">Read More &#10141;</a>
+              </div>
+            </div>
+          </div>
+          <div class="news-card">
+            <div class="image">
+              <img src="images/news/blog2.jpg" alt="">
+            </div>
+            <div class="blog-desc">
+              <div class="timestamp">
+                <span>31 March, 2024</span>
+                <span class="seperator">-</span>
+                <span>5 Comments</span>
+              </div>
+              <div class="title">
+                <h4>5 Tips For Your Job Interviews</h4>
+                <p>A job ravenously while Far much that one rank beheld after
+                outside...</p>
+                <a href="#">Read More &#10141;</a>
+              </div>
+            </div>
+          </div>
+          <div class="news-card">
+            <div class="image">
+              <img src="images/news/blog3.jpg" alt="">
+            </div>
+            <div class="blog-desc">
+              <div class="timestamp">
+                <span>30 March, 2024</span>
+                <span class="seperator">-</span>
+                <span>1 Comment</span>
+              </div>
+              <div class="title">
+                <h4>The Evening of The Holiday</h4>
+                <p>A job ravenously while Far much that one rank beheld after
+                outside...</p>
+                <a href="#">Read More &#10141;</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- SPONSORS -->
+
+      <?php   loadPartial('sponsor'); ?>
     </main>
     <!-- 
 
