d2b4314ca25b201c2f38ff495e9764e3ea038a22
Mohamed Hafez <mhafez296@gmail.com>
2024-03-31 06:42:30 +0200

    change gender to be dropdown

--- app/views/users/create.view.php
+++ app/views/users/create.view.php
@@ -11,80 +11,84 @@
       <div class="row">
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="fname" type="text" name="fname" placeholder="First Name" class="form-control w-100 py-0" value="<?= $user['fname'] ?? '' ?>" />
-            <label class="opacity-75" for="fname">First Name</label class="opacity-75">
+            <input id="fname" type="text" name="fname" placeholder="First Name" class="form-control w-100 py-1" value="<?= $user['fname'] ?? '' ?>" />
+            <label class="" for="fname">First Name</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="lname" type="text" name="lname" placeholder="last Name" class="form-control w-100 py-0" value="<?= $user['lname'] ?? '' ?>" />
-            <label class="opacity-75" for="lname">last Name</label class="opacity-75">
+            <input id="lname" type="text" name="lname" placeholder="last Name" class="form-control w-100 py-1" value="<?= $user['lname'] ?? '' ?>" />
+            <label class="" for="lname">last Name</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="email" type="text" name="email" placeholder="email " class="form-control w-100 py-0" value="<?= $user['email'] ?? '' ?>" />
-            <label class="opacity-75" for="email">email </label class="opacity-75">
+            <input id="email" type="text" name="email" placeholder="email " class="form-control w-100 py-1" value="<?= $user['email'] ?? '' ?>" />
+            <label class="" for="email">email </label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="phone_num" type="text" name="phone_num" placeholder="phone_num" class="form-control w-100 py-0" value="<?= $user['phone_num'] ?? '' ?>" />
-            <label class="opacity-75" for="phone_num">phone_num</label class="opacity-75">
+            <input id="phone_num" type="text" name="phone_num" placeholder="phone_num" class="form-control w-100 py-1" value="<?= $user['phone_num'] ?? '' ?>" />
+            <label class="" for="phone_num">phone_num</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="title" type="text" name="title" placeholder="title" class="form-control w-100 py-0" value="<?= $user['title'] ?? '' ?>" />
-            <label class="opacity-75" for="title">title</label class="opacity-75">
+            <input id="title" type="text" name="title" placeholder="title" class="form-control w-100 py-1" value="<?= $user['title'] ?? '' ?>" />
+            <label class="" for="title">title</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="bio" type="text" name="bio" placeholder="bio" class="form-control w-100 py-0" value="<?= $user['bio'] ?? '' ?>" />
-            <label class="opacity-75" for="bio">bio</label class="opacity-75">
+            <input id="bio" type="text" name="bio" placeholder="bio" class="form-control w-100 py-1" value="<?= $user['bio'] ?? '' ?>" />
+            <label class="" for="bio">bio</label class="">
           </div>
         </div>
         <div class="col-12 col-md-3">
           <div class="form-floating mb-4">
-          <input id="experience" type="number" name="experience" placeholder="experience" class="form-control w-100 py-0" value="<?= $user['experience'] ?? '' ?>" />
-            <label class="opacity-75" for="experience">experience</label class="opacity-75">
+            <input id="experience" type="number" name="experience" placeholder="experience" class="form-control w-100 py-1" value="<?= $user['experience'] ?? '' ?>" />
+            <label class="" for="experience">experience</label class="">
           </div>
         </div>
         <div class="col-12 col-md-3">
           <div class="form-floating mb-4">
-            <input id="gender" type="text" name="gender" placeholder="gender" class="form-control w-100 py-0" value="<?= $user['gender'] ?? '' ?>" />
-            <label class="opacity-75" for="gender">gender</label class="opacity-75">
+            <select id="gender" name="gender" class="form-control w-100 py-1">
+              <option value="" disabled selected>Select gender</option>
+              <option value="male" <?= isset($user['gender']) && $user['gender'] === 'male' ? 'selected' : '' ?>>Male</option>
+              <option value="female" <?= isset($user['gender']) && $user['gender'] === 'female' ? 'selected' : '' ?>>Female</option>
+            </select>
+          </div>
+        </div>
+        
+        <div class="col-12 col-md-6">
+          <div class="mb-4 d-flex align-items-center">
+            <label class="pb-3 w-auto" for="bdate">Birth Date</label>
+            <input id="bdate" type="date" name="bdate" placeholder="Birth Date" class="form-control w-100 py-1 flex-grow-1" value="<?= $user['bdate'] ?? '' ?>" max="<?= date('Y-m-d') ?>" />
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="bdate" type="date" name="bdate" placeholder="bdate" class="form-control w-100 py-0" value="<?= $user['bdate'] ?? '' ?>" />
-            <label class="opacity-75" for="bdate">bdate</label class="opacity-75">
+            <input id="country" type="text" name="country" placeholder="country" class="form-control w-100 py-1" value="<?= $user['country'] ?? '' ?>" />
+            <label class="" for="country">country</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="country" type="text" name="country" placeholder="country" class="form-control w-100 py-0" value="<?= $user['country'] ?? '' ?>" />
-            <label class="opacity-75" for="country">country</label class="opacity-75">
+            <input id="city" type="text" name="city" placeholder="city" class="form-control w-100 py-1" value="<?= $user['city'] ?? '' ?>" />
+            <label class="" for="city">city</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="city" type="text" name="city" placeholder="city" class="form-control w-100 py-0" value="<?= $user['city'] ?? '' ?>" />
-            <label class="opacity-75" for="city">city</label class="opacity-75">
+            <input id="password" type="password" name="password" placeholder="password" class="form-control w-100 py-1" value="<?= $user['password'] ?? '' ?>" />
+            <label class="text-tertiary" for="password">password</label class="">
           </div>
         </div>
         <div class="col-12 col-md-6">
           <div class="form-floating mb-4">
-            <input id="password" type="password" name="password" placeholder="password" class="form-control w-100 py-0" value="<?= $user['password'] ?? '' ?>" />
-            <label class="text-tertiary" for="password">password</label class="opacity-75">
-          </div>
-        </div>
-        <div class="col-12 col-md-6">
-          <div class="form-floating mb-4">
-            <input id="password_confirmation" type="password" name="password_confirmation" placeholder="password_confirmation" class="form-control w-100 py-0" value="<?= $user['password_confirmation'] ?? '' ?>" />
-            <label class="text-tertiary" for="password_confirmation">password confirmation</label class="opacity-75">
+            <input id="password_confirmation" type="password" name="password_confirmation" placeholder="password_confirmation" class="form-control w-100 py-1" value="<?= $user['password_confirmation'] ?? '' ?>" />
+            <label class="text-tertiary" for="password_confirmation">password confirmation</label class="">
           </div>
         </div>
 
